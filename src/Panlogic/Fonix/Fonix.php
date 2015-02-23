4aed9fa3016fed675f130f6f1570a27cfc43b9c0
David Wright <dave@ritey.com>
2015-02-23 14:39:12 +0000

    License file update and tweaks

--- src/Panlogic/Fonix/Fonix.php
+++ src/Panlogic/Fonix/Fonix.php
@@ -126,6 +126,35 @@ class Fonix {
 	}
 
 	/**
+	 * Convert a binary expression (e.g., "100111") into a binary-string
+	 *
+	 * @return String
+	 */
+	function bin2bstr($input)
+	{
+	  if (!is_string($input)) return null; // Sanity check
+
+	  // Pack into a string
+	  return pack('H*', base_convert($input, 2, 16));
+	}
+
+	/**
+	 * Binary representation of a binary-string
+	 *
+	 * @return String
+	 */
+	function bstr2bin($input)
+	{
+	  if (!is_string($input)) return null; // Sanity check
+
+	  // Unpack as a hexadecimal string
+	  $value = unpack('H*', $input);
+
+	  // Output binary representation
+	  return base_convert($value[1], 16, 2);
+	}
+
+	/**
 	 * Make a request
 	 *
 	 * @return Object
@@ -133,6 +162,12 @@ class Fonix {
 	private function call()
 	{
 		$this->requestOptions['body'] = array_merge($this->requestOptions['body'], ['ORIGINATOR' => $this->originator]);
+		$requestOptions = [];
+		foreach($this->requestOptions['body'] as $key=>$value)
+		{
+			$requestOptions[strtoupper($key)] = $value;
+		}
+		$this->requestOptions['body'] = $requestOptions;
 		try
 		{
 			switch(strtolower($this->method))
@@ -174,12 +209,20 @@ class Fonix {
 	 */
 	private function response($response)
 	{
-		$result = new stdClass();
+		$result = new \stdClass();
 		$body = $response->getBody();
 		$result->statusCode = $response->getStatusCode();
 		$result->reason = $response->getReasonPhrase();
-		$result->json = $body->json();
-		$result->xml = $body->xml();
+		$result->json = '';
+		if ($this->responseFormat == 'json')
+		{
+			$result->json = $response->json();
+		}
+		$result->xml = '';
+		if ($this->responseFormat == 'xml')
+		{
+			$result->xml = $response->xml();
+		}
 		$result->body = $body;
 		return $result;
 	}
@@ -191,7 +234,7 @@ class Fonix {
 	 */
 	private function basicMOResponse()
 	{
-		$return = new stdClass();
+		$return = new \stdClass();
 		$return->ifversion = isset($_POST['IFVERSION']) ? $_POST['IFVERSION'] : '';
 		$return->monumber = isset($_POST['MONUMBER']) ? $_POST['MONUMBER'] : '';
 		$return->operator = isset($_POST['OPERATOR']) ? $_POST['OPERATOR'] : '';
@@ -208,10 +251,11 @@ class Fonix {
 	 * @param  array  $body
 	 * @return Object
 	 */
-	public function sendSMS($body = array())
+	public function sendSMS($body = array(), $originator = null)
 	{
 		$this->requestOptions['body'] = $body;
 		$this->endpoint = 'sendsms';
+		$this->setOriginator($originator);
 		return $this->response($this->call());
 	}
 
@@ -221,10 +265,11 @@ class Fonix {
 	 * @param  array  $body
 	 * @return Object
 	 */
-	public function chargeSMS($body = array())
+	public function chargeSMS($body = array(), $originator = null)
 	{
 		$this->requestOptions['body'] = $body;
-		$this->endpoint = 'sendsms';
+		$this->endpoint = 'chargesms';
+		$this->setOriginator($originator);
 		return $this->response($this->call());
 	}
 
@@ -234,10 +279,12 @@ class Fonix {
 	 * @param  array  $body
 	 * @return Object
 	 */
-	public function sendBinarySMS($body = array())
+	public function sendBinarySMS($body = array(), $originator = null)
 	{
-		$this->requestOptions['binbody'] = $body;
+		$this->requestOptions['body'] = array_merge($body, array('BINBODY' => isset($body['body']) ? $this->bstr2bin($body['body']) : '' ,'BINHEADER' => isset($body['header']) ? $this->bstr2bin($body['header']) : ''));
+		unset($this->requestOptions['body']['body']);
 		$this->endpoint = 'sendbinsms';
+		$this->setOriginator($originator);
 		return $this->response($this->call());
 	}
 
@@ -247,11 +294,14 @@ class Fonix {
 	 * @param  array  $body
 	 * @return Object
 	 */
-	public function sendWapPush($body = array())
+	public function sendWapPush($body = array(), $originator = null)
 	{
-		$this->requestOptions['pushtitle'] = isset($body['title']) ? $body['title'] : '';
-		$this->requestOptions['pushlink'] = isset($body['link']) ? $body['link'] : '';
+		$this->requestOptions['body'] = array_merge($body, array('PUSHTITLE' => isset($body['title']) ? $body['title'] : '' ,'PUSHLINK' => isset($body['link']) ? $body['link'] : ''));
+		unset($this->requestOptions['body']['body']);
+		unset($this->requestOptions['body']['title']);
+		unset($this->requestOptions['body']['link']);
 		$this->endpoint = 'sendwappush';
+		$this->setOriginator($originator);
 		return $this->response($this->call());
 	}
 
@@ -354,5 +404,20 @@ class Fonix {
 	public function setResponseFormat($format = "json")
 	{
 		$this->responseFormat = $format;
+	}
+
+	/**
+	 * Set the originator
+	 *
+	 * @param string
+	 * @return void
+	 *
+	 */
+	public function setOriginator($originator = null)
+	{
+		if(!is_null($originator))
+		{
+			$this->originator = $originator;
+		}
 	}
 }
